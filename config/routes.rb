5ed5dc213b2f81671a91ae59ee15c259ab9ccf35
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-08-29 16:01:55 +0900

    バリデーション修正など

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get 'index', to: 'index#index'
+
+# get 'sign_in(new_user_session)  sessions#new
+
 
 end
