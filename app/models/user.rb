e1161cabf1bb41818949f3fd703d7385cdd3de61
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-09-01 14:04:52 +0900

    エラーメッセージ表示完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
     validates :first_huri, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :family_huri, uniqueness: true, format: { with: /\A[ァ-ヶー－]+\z/}
-    # validates :birthday,    format: { with: /\A[a-zA-Z0-9]+\z/ }
+    # validates :birthday,    format: { with: /\A[0-9]+\z/ }
   # end
 
   has_many   :items
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'toppage/index', to: 'items/index#index'
-# post 'users', to: 'devise/registrations#create'
 get 'items/registration', to: 'items/new#new'
-# delete 'users/sign_out', to: 'items/index#index'
 end
