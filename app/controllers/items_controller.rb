0187cc1224197d70e66f7b4e7fef26abf01374b8
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-09-03 14:32:49 +0900

    item/newファイルのカラム名を追加。その他不足ファイルの追加。ルーティングの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in, except: [:index]
+  # before_action :move_to_sign_in, except: [:index]
 
   def index
   end
--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,12 +1,15 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 
-window.addEventListener('', function(){
+window.addKeyListener('', function(){
 
-  const calcTax = document.getElementById("item-price")
+  const calcTax = document.getElementById("price-input")
 
-  calcTax.addEventListener('keyevent', function(){
-
+  calcTax.addKeyListener('load', function(){
+    getKeyChar
+    input type="number"
+    'price-input' * 0.1 = 'add-tax-price'
+    'price-input' - 'add-tax-price' = 'profit'
   })
 
 })
