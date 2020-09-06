634ad203f496ebe5a5ee27f2d2eadc004891a953
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-09-07 01:30:10 +0900

    外部キーを呼び出せそうな形をある程度作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
   def index
     query = "SELECT * FROM items"
     @items = Item.find_by_sql(query)
+  
   end
 
   def new
@@ -22,6 +23,7 @@ class ItemsController < ApplicationController
     end
   end
 
+
   # def move_to_index（備忘録として残します）
   #   unless user_signed_in?
   #     redirect_to action: :index
@@ -36,4 +38,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:pro_name, :explanation, :category_id, :condition_id, :deli_money_id, :deli_time_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def check_sold_out
+    params.require(:buying).permit(:item.pro_name)
+  end
 end
