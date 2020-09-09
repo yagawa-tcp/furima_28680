47128a0f9a790b352be11dd6ea15e5c0aa69e8af
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-09-09 14:59:19 +0900

    ミス

--- app/controllers/buyings_controller.rb
+++ app/controllers/buyings_controller.rb
@@ -1,4 +1,28 @@
 class BuyingsController < ApplicationController
 
+  before_action :finder, only: [:index]
+  before_action :authenticate_user!
+
+  def index
+    @buy
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    elsif @item.buying.present?
+      redirect_to root_path
+    end
+  end
+
+  def create
+    if @buying.create
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def finder
+    @item = Item.find(params[:item_id])
+  end
+
 
 end
